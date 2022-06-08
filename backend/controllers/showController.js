58dcc0207df584c7637049060fd6f6fe88198555
ozan isik <ozan_isik14@hotmail.com>
2022-06-08 01:44:50 -0400

    [build][backend] connect db and initial showModel

--- backend/controllers/showController.js
+++ backend/controllers/showController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc    Get all shows
 //@route   GET /api/shows/
 //@access  Private
-const getShows = (req, res) => {
+const getShows = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'get shows'})
-}
+})
 
 //@desc    Set shows
 //@route   POST /api/shows/
 //@access  Private
-const setShows = (req, res) => {
+const setShows = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a new text field')
     }
     res.status(200).json({ message: 'set show'})
-}
+})
 
 //@desc    Update shows
 //@route   PUT /api/shows/:id
 //@access  Private
-const updateShow = (req, res) => {
+const updateShow = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `updated show ${req.params.id}`})
-}
+})
 
 //@desc    Delete shows
 //@route   DELETE /api/shows/:id
 //@access  Private
-const deleteShow = (req, res) => {
+const deleteShow = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `deleted show ${req.params.id}`})
-}
+})
 
 module.exports = {
     getShows,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
 // Configure express
 app.use(express.json())
+// Accepting body data
 app.use(express.urlencoded({ extended: false }))
 
 // Configure express routes
