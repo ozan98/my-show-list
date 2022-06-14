18be2453b6463faf6f62120bfea457a2ce14b769
ozan isik <ozan_isik14@hotmail.com>
2022-06-13 22:17:28 -0400

    [build][frontend] header navigation and initial register page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <div>Header</div>
+        <header className="header">
+            <div>
+                <Link to="/">My Show List</Link>
+            </div>
+
+            <div>
+                <Link to="/dashboard">My List</Link>
+            </div>
+
+            <div>
+                <Link to="/login">Login</Link>
+            </div>
+
+            <div>
+                <Link to="/register">Register</Link>
+            </div>
+        </header>
     )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 
 function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password1: '',
+        password2: '',
+    })
+
+    const {name, email, password1, password2} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
     return (
-        <div>Register</div>
+        <>
+            <section className="heading">
+                <h1>Register</h1>
+                <p>Please add all fields to register</p>
+            </section>
+
+            <section>
+                <form>
+                    <div className="form-group">
+                    <div>
+                        <input 
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={name}
+                            placeHolder="Enter name"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeHolder="Enter email"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div>
+                        <input 
+                            type="password"
+                            id="password1"
+                            name="password1"
+                            value={password1}
+                            placeHolder="Enter password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div>
+                        <input 
+                            type="password"
+                            id="password2"
+                            name="password2"
+                            value={password2}
+                            placeHolder="Enter password"
+                            onChange={onChange}
+                        />
+                    </div>
+                    </div>
+
+                    <div className="form-group">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
 
