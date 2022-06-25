bfaca52c1103d9ca662a07921115cba13e848b58
ozan isik <ozan_isik14@hotmail.com>
2022-06-24 23:26:53 -0400

    [build][frontend] add media to user and retrieve all media for user

---
+++ frontend/src/components/UserMediaCard.jsx
@@ -0,0 +1,13 @@
+function UserMediaCard({name, image, mediaType, score, status}) {
+    return (
+        <div className="user-media-card">
+            <img src={image}/>
+            <p>{name}</p>
+            <p>{mediaType}</p>
+            <p>{score}</p>
+            <p>{status}</p>
+        </div>
+    )
+}
+
+export default UserMediaCard
--- frontend/src/pages/MyList.jsx
+++ frontend/src/pages/MyList.jsx
@@ -1,3 +1,5 @@
+import util from '../util/util'
+import UserMediaCard from '../components/UserMediaCard'
 import {useEffect} from 'react'
 import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router'
@@ -5,7 +7,21 @@ import { useNavigate } from 'react-router'
 function MyList() {
     const navigate = useNavigate()
     const {user} = useSelector((state) => state.auth)
+    const {medias} = useSelector((state) => state.media)
     
+    const getmedias = () => {
+        return medias.map((media) => {
+            return <UserMediaCard
+                        key={media.id}
+                        name={media.title}
+                        image={util.getImage(media.imagePath)}
+                        mediaType={media.mediaType}
+                        score={media.score}
+                        status={media.status}
+                    />
+        })
+    }
+
     useEffect(() => {
         if(!user) {
             navigate('/login')
@@ -13,7 +29,10 @@ function MyList() {
     },[user])
 
     return (
-        <div>My List</div>
+        <>
+            My List
+            {getmedias()}
+        </>
     )
 }
 
