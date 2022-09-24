9116fce22b06199333779796f02d0590850459a6
ozan isik <ozan_isik14@hotmail.com>
2022-09-24 15:47:23 -0400

    [build][frontend] display user media list of different status

--- frontend/src/pages/MyList.jsx
+++ frontend/src/pages/MyList.jsx
@@ -1,6 +1,6 @@
 import util from '../util/util'
 import UserMediaCard from '../components/UserMediaCard'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import { useNavigate } from 'react-router'
 import {getAllMedia} from '../features/media/mediaSlice'
@@ -12,15 +12,31 @@ function MyList() {
     const {user} = useSelector((state) => state.auth)
     const {medias} = useSelector((state) => state.media)
 
+    const [userListMedia, setUserListMedia] = useState([])
+
     useEffect(() => {
         dispatch(getAllMedia())
     },[])
     
-    const getmedias = () => {
-        return medias.map((media) => {
+    // const getmedias = () => {
+    //     return medias.map((media) => {
+    //         return <UserMediaCard
+    //                     key={media._id}
+    //                     id={media._id}
+    //                     name={media.title}
+    //                     image={util.getImage(media.imagePath)}
+    //                     mediaType={media.mediaType}
+    //                     score={media.score}
+    //                     status={media.status}
+    //                 />
+    //     })
+    // }
+
+    const renderMedias = () => {
+        return userListMedia.map((media) =>{
             return <UserMediaCard
                         key={media._id}
-                        id={media._id}
+                        id={media.id}
                         name={media.title}
                         image={util.getImage(media.imagePath)}
                         mediaType={media.mediaType}
@@ -30,16 +46,58 @@ function MyList() {
         })
     }
 
+
+
     useEffect(() => {
         if(!user) {
             navigate('/login')
         }
     },[user])
 
+    const setMedia = (status)=> {
+        const list = medias.filter((media) =>{
+            return media.status === status
+        })
+        setUserListMedia(list)
+    }
+
+    const getMediaStatusList = (status) => {
+        switch(status) {
+            case 'currently watching':
+                setMedia('currently watching')
+                break;
+            case 'completed':
+                setMedia('completed')
+                break;
+            case 'on hold':
+                setMedia('on hold')
+                break;
+            case 'dropped':
+                setMedia('dropped')
+                break;
+            case 'plan to watch':
+                setMedia('plan to watch')
+                break;
+            case 'all medias':
+                setUserListMedia(medias)
+            default:
+                setUserListMedia(medias)
+
+        }
+    }
+
     return (
         <>
-            My List
-            {getmedias()}
+            <h1>My List</h1>
+            <div className="my-list-buttons">
+                <button onClick={()=> getMediaStatusList('all medias')}>All medias</button>
+                <button onClick={() => getMediaStatusList('started')}>Currently watching</button>
+                <button onClick={() => getMediaStatusList('started')}>Completed</button>
+                <button onClick={() => getMediaStatusList('started')}>On hold</button>
+                <button onClick={() => getMediaStatusList('started')}>Dropped</button>
+                <button onClick={() => getMediaStatusList('started')}>Plan to watch</button>
+            </div>
+            {renderMedias()}
         </>
     )
 }
