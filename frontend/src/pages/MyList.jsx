3cd8ea70da5d49cdcba58a261b579d7e44071a31
ozan isik <ozan_isik14@hotmail.com>
2022-10-05 21:34:14 -0400

    [build][frontend] filtering media list in my list page

--- frontend/src/pages/MyList.jsx
+++ frontend/src/pages/MyList.jsx
@@ -12,56 +12,25 @@ function MyList() {
     const {user} = useSelector((state) => state.auth)
     const {medias} = useSelector((state) => state.media)
 
-    const [userListMedia, setUserListMedia] = useState([])
+    const [typeFilter, setTypeFilter] = useState('all')
+    const [mediaStatusFilter, setMediaStatusFilter] = useState('all')
 
-    // useEffect(() => {
-    //     dispatch(getAllMedia())
-    // },[])
-    
-    // const getmedias = () => {
-    //     return medias.map((media) => {
-    //         return <UserMediaCard
-    //                     key={media._id}
-    //                     id={media._id}
-    //                     name={media.title}
-    //                     image={util.getImage(media.imagePath)}
-    //                     mediaType={media.mediaType}
-    //                     score={media.score}
-    //                     status={media.status}
-    //                 />
-    //     })
-    // }
+    console.log(typeFilter)
+    console.log(mediaStatusFilter)
 
     useEffect(() => {
         if(!user) {
             navigate('/login')
         }
-
-        // if(userListMedia.length === 0){
-        //     dispatch(getAllMedia())
-        //     setUserListMedia([...medias])
-        //     console.log(userListMedia)
-        // }else {
-        //     console.log('else')
-        // }
-            dispatch(getAllMedia())
-            setUserListMedia([...medias])
-        
-
-        // if(medias.length === 0) {
-        //     dispatch(getAllMedia())
-        //     setUserListMedia(medias)
-        // }
-
-        // if(medias.length > 0) {
-        //     setUserListMedia(medias)
-        // }
+        dispatch(getAllMedia())
 
     },[user])
 
-    const renderMedias = (medias) => {
-        console.log(medias)
-        return medias.map((media) =>{
+    const renderMedias = (list) => {
+
+        const filteredList = filterList(list)
+
+        return filteredList.map((media) =>{
             return <UserMediaCard
                         key={media._id}
                         id={media.id} 
@@ -74,55 +43,40 @@ function MyList() {
         })
     }
 
+    const filterList = (list) => {
 
-
-    
-
-    const setMedia = (status)=> {
-        const list = medias.filter((media) =>{
-            return media.status === status
-        })
-        setUserListMedia(list)
-    }
-
-    const getMediaStatusList = (status) => {
-        switch(status) {
-            case 'currently watching':
-                setMedia('currently watching')
-                break;
-            case 'completed':
-                setMedia('completed')
-                break;
-            case 'on hold':
-                setMedia('on hold')
-                break;
-            case 'dropped':
-                setMedia('dropped')
-                break;
-            case 'plan to watch':
-                setMedia('plan to watch')
-                break;
-            case 'all medias':
-                setUserListMedia(medias)
-                break;
-            default:
-                setUserListMedia(medias)
-
+        if(typeFilter === 'tv' || typeFilter === 'movie'){
+            return medias.filter((media) =>{
+                return ((media.status === mediaStatusFilter) && (media.mediaType === typeFilter))
+                        || (media.mediaType === typeFilter)
+            })
+        }else {
+            return medias.filter((media) =>{
+                return  (mediaStatusFilter === 'all') || (media.status === mediaStatusFilter)
+            })
         }
+
+
     }
+
 
     return (
         <>
             <h1>My List</h1>
             <div className="my-list-buttons">
-                <button onClick={()=> getMediaStatusList('all medias')}>All medias</button>
-                <button onClick={() => getMediaStatusList('currently watching')}>Currently watching</button>
-                <button onClick={() => getMediaStatusList('completed')}>Completed</button>
-                <button onClick={() => getMediaStatusList('on hold')}>On hold</button>
-                <button onClick={() => getMediaStatusList('dropped')}>Dropped</button>
-                <button onClick={() => getMediaStatusList('plan to watch')}>Plan to watch</button>
+                <button onClick={() => setTypeFilter('all')}>All Media</button>
+                <button onClick={() => setTypeFilter('tv')}>Tv Shows</button>
+                <button onClick={() => setTypeFilter('movie')}>Movies</button>
             </div>
-            {userListMedia.length > 0 ? renderMedias(userListMedia) : renderMedias(medias)}
+            <div className="my-list-buttons">
+                <button onClick={()=> setMediaStatusFilter('all')}>All medias</button>
+                <button onClick={() => setMediaStatusFilter('currently watching')}>Currently Watching</button>
+                <button onClick={() => setMediaStatusFilter('completed')}>Completed</button>
+                <button onClick={() => setMediaStatusFilter('on hold')}>On Hold</button>
+                <button onClick={() => setMediaStatusFilter('dropped')}>Dropped</button>
+                <button onClick={() => setMediaStatusFilter('plan to watch')}>Plan To Watch</button>
+            </div>
+            {renderMedias(medias)}
         </>
     )
 }
