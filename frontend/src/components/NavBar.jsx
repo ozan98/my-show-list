3b3fd55818b0a91dc383fcc306f306b926c4f960
ozan isik <ozan_isik14@hotmail.com>
2022-10-11 21:54:41 -0400

    [build][style][frontend] mobile-nav slide out menu

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
 import {useSelector, useDispatch} from 'react-redux'
 import {logout, reset} from '../features/auth/authSlice'
 import {resetMedia} from '../features/media/mediaSlice'
+import {useState, useEffect} from 'react'
 
 
 function NavBar() {
@@ -10,6 +11,13 @@ function NavBar() {
     const navigate = useNavigate()
     
     const {user} = useSelector((state) => state.auth)
+    const [mobileMenuToggle, setMobileToggle] = useState(true)
+
+    useEffect(() => {
+        return () => {
+            setMobileToggle(false)
+        }
+    },[])
 
     const onLogout = () => {
         dispatch(logout())
@@ -18,9 +26,17 @@ function NavBar() {
         navigate('/')
     }
 
+    const toggleMenu = () => {
+        if(mobileMenuToggle === false) {
+            setMobileToggle(true)
+        } else {
+            setMobileToggle(false)
+        }
+    }
+
     return (
             <>
-            <nav>
+            <nav className="navbar">
             <div className="navbar-container">
                 <div>
                     <Link className="logo" to="/"> My Show List</Link>
@@ -50,13 +66,39 @@ function NavBar() {
                 </div>
     
 
-            <div className="hamburger">
+            <div className="hamburger" onClick={toggleMenu}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
             
             </div>
+            </nav>
+
+
+
+
+            <nav className={`mobile-nav ${mobileMenuToggle === true ? "mobile-nav-active" : ""}`}>
+                    <Link className="mobile-link" onClick={() => setMobileToggle(false)} to="/mylist">My List</Link>
+                    <Link className="mobile-link" onClick={() => setMobileToggle(false)} to="/discover">Discover</Link>
+                    {user ? (
+                        <>
+                            <div className="mobile-link" onClick={() => setMobileToggle(false)} onClick={onLogout}>
+                                <FaSignOutAlt />Logout
+                            </div>
+                        </>
+                    ) :
+                    (
+                        <>
+                            <Link className="mobile-link" onClick={() => setMobileToggle(false)} to="/login">
+                                <FaSignInAlt /> Login
+                            </Link>
+
+                            <Link className="mobile-link" onClick={() => setMobileToggle(false)} to="/register">
+                                <FaUser /> Register
+                            </Link>
+                        </>
+                    )}
             </nav>
             
             </>
