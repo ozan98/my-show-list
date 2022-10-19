94777319c10751e6407cc9f955ab712dc38ab36a
ozan isik <ozan_isik14@hotmail.com>
2022-10-19 01:56:46 -0400

    push for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,9 @@ app.use('/api/users/', require('./routes/userRoutes'))
 
 // Serve frontend
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build'), {
-        dotfiles: 'allow'
-    }))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build/index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 }
 
 // This middle ware will replace express error handler
